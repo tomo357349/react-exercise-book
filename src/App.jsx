88217077d016aed20228826d5d17aba0ce79c41f
tomo357349 <usctakahashi@hotmail.com>
2025-02-08 10:55:45 +0900

    viteで開発環境を構築し、とりあえず最初の出力ができるところまで

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+	return (
+        <>
+            <h1>Hello World!</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ullam commodi, natus dicta at nisi maxime tempora aspernatur modi, laboriosam, obcaecati repellat saepe quae. Exercitationem hic atque facilis enim consequuntur!</p>
+        </>
+    );
+}
